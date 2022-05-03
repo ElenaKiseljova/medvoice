8b1dfd58b1c4e6e7381e621b92bf64656b9b0ec0
ElenaKiseljova <e.a.kiseljova@gmail.com>
2022-05-03 21:39:05 +0300

    - добавлена ф-я для добавления языковых версий терминам

--- front-page.php
+++ front-page.php
@@ -8,6 +8,48 @@
   get_header(  );
 ?>
 
+<?php 
+  //sections, authors, labels
+  $terms = get_terms( array(
+    'taxonomy' => ['sections', 'authors', 'labels'],
+    'hide_empty' => false,
+  ) );
+
+  // var_dump($terms);
+  $langs = pll_languages_list( ['hide_empty' => false]);
+
+  foreach ($terms as $key => $term) {
+    $translates = pll_get_term_translations($term->term_id);
+    $translates_keys = array_keys($translates);
+
+    var_dump($translates);
+    echo '<br><br><br><br>';
+
+    if (count($translates_keys) < count($langs)) {
+      echo $term->name;
+
+      foreach ($langs as $key => $lang) {
+        if (array_search($lang, $translates_keys) === false) {
+          
+          $term_insert = wp_insert_term( $term->name . '-' . $lang, $term->taxonomy );
+        
+          if ( !is_wp_error($term_insert) ) {
+            $new_term_id = $term_insert['term_id'];
+
+            pll_set_term_language($new_term_id, $lang);
+
+            $translates_new = [$lang => $new_term_id ];
+
+            $translates = array_merge($translates, $translates_new);
+          }
+        }  
+      }
+
+      pll_save_term_translations($translates);
+    }
+  }
+?>
+
 
 <main class="main"> 
   <?php 
